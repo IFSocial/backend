1a517d7c1d0a49a411f3a52e70471bd89224e982
marques <marqueschristmann8@gmail.com>
2022-11-10 21:04:29 -0300

    headers update

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 
-import routes from './routes'
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(routes)
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+    'Access-Control-Allow-Origin',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: false,
+};
+
+app.use(express.json());
+app.use(routes);
+
+app.use(cors(options));
 
 app.listen(process.env.PORT, () => {
   console.log('Running on Port 5000');
-})
+});
+
